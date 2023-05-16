cc62103d656225eed32a5dfc7782a949d5259090
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-16 20:19:02 +0100

    Bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@ Base class declaration module for other classes
 
 from uuid import uuid4
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -24,6 +23,7 @@ class BaseModel:
             and it will be updated every time you change your object
         """
 
+        from models import storage
         if not kwargs:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -50,6 +50,7 @@ class BaseModel:
         updates the public instance attribute updated_at with the
         current datetime
         """
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
