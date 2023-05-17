d8f04bf0515a17ff1dc0d9d83efed48d54bbceec
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-17 07:30:12 +0100

    Updated the place model

---
+++ models/place.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Declare the Place Model extending the 
+BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    Declare the Place Model
+
+    Arguments:
+        city_id: string - empty string: it will be the City.id
+        user_id: string - empty string: it will be the User.id
+        name: string - empty string
+        description: string - empty string
+        number_rooms: integer - 0
+        number_bathrooms: integer - 0
+        max_guest: integer - 0
+        price_by_night: integer - 0
+        latitude: float - 0.0
+        longitude: float - 0.0
+        amenity_ids: list of string - empty list: it will be the list of Amenity.id later
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
