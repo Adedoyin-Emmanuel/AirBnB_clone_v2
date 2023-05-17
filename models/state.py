a26f73d7ecd521ea31dacf716c771f4212630286
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-17 07:23:20 +0100

    Updated the states model

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""The State model extending the base model"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Shows the State model for any BaseModel object"""
+    name = ""
