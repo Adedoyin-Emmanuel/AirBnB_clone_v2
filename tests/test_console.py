0df5ee47771b8c04e7eea9952e38ecf04aae3a84
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-18 08:08:24 +0100

    Bug fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,32 +19,77 @@ class ConsoleTestCase(unittest.TestCase):
         sys.stdout = sys.__stdout__
         return captured_output.getvalue().strip()
 
-    def test_create(self):
+    def test_create_missing_class(self):
+        result = self.capture_stdout(self.console.onecmd, "create")
+        self.assertEqual(result, "** class name missing **")
+
+    def test_create_invalid_class(self):
+        result = self.capture_stdout(
+            self.console.onecmd, "create InvalidClass")
+        self.assertEqual(result, "** class doesn't exist **")
+
+    def test_create_valid_class(self):
         result = self.capture_stdout(self.console.onecmd, "create User")
         self.assertIsNotNone(result)  # Check if output is not empty
 
-    def test_show(self):
+    def test_show_missing_class(self):
+        result = self.capture_stdout(self.console.onecmd, "show")
+        self.assertEqual(result, "** class name missing **")
+
+    def test_show_missing_id(self):
+        result = self.capture_stdout(self.console.onecmd, "show User")
+        self.assertEqual(result, "** instance id missing **")
+
+    def test_show_invalid_id(self):
         result = self.capture_stdout(self.console.onecmd, "show User 123")
         self.assertEqual(result, "** no instance found **")
 
-    def test_all(self):
-        result = self.capture_stdout(self.console.onecmd, "all")
+    def test_all_invalid_class(self):
+        result = self.capture_stdout(self.console.onecmd, "all InvalidClass")
+        self.assertEqual(result, "** class doesn't exist **")
+
+    def test_all_valid_class(self):
+        result = self.capture_stdout(self.console.onecmd, "all User")
         self.assertIsNotNone(result)  # Check if output is not empty
 
-    def test_destroy(self):
+    def test_destroy_missing_class(self):
+        result = self.capture_stdout(self.console.onecmd, "destroy")
+        self.assertEqual(result, "** class name missing **")
+
+    def test_destroy_missing_id(self):
+        result = self.capture_stdout(self.console.onecmd, "destroy User")
+        self.assertEqual(result, "** instance id missing **")
+
+    def test_destroy_invalid_id(self):
         result = self.capture_stdout(self.console.onecmd, "destroy User 123")
         self.assertEqual(result, "** no instance found **")
 
-    def test_update(self):
+    def test_update_missing_class(self):
+        result = self.capture_stdout(self.console.onecmd, "update")
+        self.assertEqual(result, "** class name missing **")
+
+    def test_update_missing_id(self):
+        result = self.capture_stdout(self.console.onecmd, "update User")
+        self.assertEqual(result, "** instance id missing **")
+
+    def test_update_missing_dictionary(self):
+        result = self.capture_stdout(self.console.onecmd, 'User.update("123")')
+        self.assertEqual(result, "** no instance found **")
+
+    def test_update_invalid_id(self):
         result = self.capture_stdout(
             self.console.onecmd, 'User.update("123", {"first_name": "Emma"})')
         self.assertEqual(result, "** no instance found **")
 
-    def test_count(self):
-        result = self.capture_stdout(self.console.onecmd, "count User")
-        self.assertIsNotNone(result)  # Check if output is not empty
-        self.assertTrue(result.isdigit())  # Check if output is a number
+    def test_update_invalid_dictionary(self):
+        result = self.capture_stdout(
+            self.console.onecmd, 'User.update("123", "invalid")')
+        self.assertEqual(result, "** no instance found **")
 
+    def test_update_valid_dictionary(self):
+        result = self.capture_stdout(
+            self.console.onecmd, 'User.update("5610c8e7-f06f-48e7-afae-a02adc959787", {"first_name": "Emma"})')
+        self.assertEqual(result, "Instance updated successfully.")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_update_missing_attribute_value(self):
+        result = self
